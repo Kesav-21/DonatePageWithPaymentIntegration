d2ce6767603f671e173898cda8536bc6c7feb94b
Kesav-21 <cgkesav@gmail.com>
2023-02-13 16:27:02 +0530

    Initial

---
+++ src/Component/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Header=()=>{
+    return(
+        <header id="header" class="header-section">
+    <div class="top-header">
+        <div class="container">
+            <div class="top-content-wrap row">
+                <div class="col-sm-8">
+                    <ul class="left-info">
+                        <li><a href="#"><i class="ti-email"></i>Info@YourDomain.com</a></li>
+                        <li><a href="#"><i class="ti-mobile"></i>+(333) 052 39876</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 d-none d-md-block">
+                    <ul class="right-info">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+    )
+}
+
+export default Header;
